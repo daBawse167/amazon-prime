b1f1a3f4aa7b51555228d75e0dd8f6dabad4f8c1
Alex Hadjiev <36988416+daBawse167@users.noreply.github.com>
2021-11-20 18:18:16 +0000

    Update app.py

--- app.py
+++ app.py
@@ -41,6 +41,8 @@ def get_data():
     if message==['', '', '', '', '', '', '']:
         return render_template("home.html", result1="Please enter a value in any of the text boxes")
     
+    return render_template("home.html", result1=message)
+    
     features = np.array([["listed_in", "rating", "release_year", "duration", "cast", "director", "country"], message])
     non_empty = [x for x in features[1] if x != ""]
     non_empty_idx = [list(features[1]).index(x) for x in non_empty]
