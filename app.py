b6ae5f296b5f0ced6597d943c0c04cbe1a149dfa
Alex Hadjiev <36988416+daBawse167@users.noreply.github.com>
2021-11-20 17:06:55 +0000

    Update app.py

--- app.py
+++ app.py
@@ -33,57 +33,7 @@ def home():
 
 @app.route("/get_data", methods=["POST"])
 def get_data():
-    
-    if [x for x in request.form.values()]==['', '', '', '', '', '', '']:
-        return render_template("home.html", result1="Please enter a value in any of the text boxes")
-    
-    features = np.array([["listed_in", "rating", "release_year", "duration", "cast", "director", "country"],
-                         [x for x in request.form.values()]])
-    non_empty = [x for x in features[1] if x != ""]
-    non_empty_idx = [list(features[1]).index(x) for x in non_empty]
-    features = features.T[non_empty_idx]
-    
-    if "duration" in features.T[0]:
-        idx = list(features.T[0]).index("duration")
-        input_ = features.T[1][idx]
-        
-        if not input_.isnumeric():
-            return render_template("home.html", result1="Please enter the duration as a number")
-        else:
-            features[idx][1] = input_+" min"
-            
-    if "release_year" in features.T[0]:
-        idx = list(features.T[0]).index("release_year")
-        input_ = features.T[1][idx]
-        
-        if not input_.isnumeric():
-            return render_template("home.html", result1="Please enter the release year as a number")
-    
-    result = get_features(features)
-    input_features = "Results for "+", ".join([str(item) for item in features.T[0]])
-    
-    if len(result)==0:
-        return render_template("home.html", result1="Your input did not match any movie or TV show in the database")
-    elif len(result)==1:
-        return render_template("home.html", input_features=input_features, result1=result[0])
-    elif len(result)==2:
-        return render_template("home.html", input_features=input_features, result1=result[0], result2=result[1])
-    elif len(result)==3:
-        return render_template("home.html", input_features=input_features, result1=result[0], result2=result[1], result3=result[2])
-    elif len(result)==4:
-        return render_template("home.html", input_features=input_features, result1=result[0], result2=result[1], result3=result[2], result4=result[3])
-    elif len(result)==5:
-        return render_template("home.html", input_features=input_features, result1=result[0], result2=result[1], result3=result[2], result4=result[3], result5=result[4])
-    elif len(result)==6:
-        return render_template("home.html", input_features=input_features, result1=result[0], result2=result[1], result3=result[2], result4=result[3], result5=result[4], result6=result[5])
-    elif len(result)==7:
-        return render_template("home.html", input_features=input_features, result1=result[0], result2=result[1], result3=result[2], result4=result[3], result5=result[4], result6=result[5], result7=result[6])
-    elif len(result)==8:
-        return render_template("home.html", input_features=input_features, result1=result[0], result2=result[1], result3=result[2], result4=result[3], result5=result[4], result6=result[5], result7=result[6], result8=result[7])
-    elif len(result)==9:
-        return render_template("home.html", input_features=input_features, result1=result[0], result2=result[1], result3=result[2], result4=result[3], result5=result[4], result6=result[5], result7=result[6], result8=result[7], result9=result[8])
-    elif len(result)>=10:
-        return render_template("home.html", input_features=input_features, result1=result[0], result2=result[1], result3=result[2], result4=result[3], result5=result[4], result6=result[5], result7=result[6], result8=result[7], result9=result[8], result10=result[9])
+    return render_template(result1=request.form.values())
     
 if __name__=='__main__':
     app.run(debug=True)
