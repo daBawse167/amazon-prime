f55b2600240e6b512fa4b55bf40010490d3cc28f
Alex Hadjiev <36988416+daBawse167@users.noreply.github.com>
2021-11-21 20:05:05 +0000

    Update app.py

--- app.py
+++ app.py
@@ -44,17 +44,14 @@ def get_data():
         return render_template("home.html", result1="Please enter a value in any of the text boxes")
     
     if len(message) != 7:
-        uniques = [np.unique(df[name]) for name in feature_names]
-        feats = []
-
         for msg in message:
-            for unique_list in uniques:
+            for name in feature_names:
+                unique_list = list(pd.read_csv(name+".csv"))
                 if msg in unique_list:
                     idx = uniques.index(unique_list) 
                     feats.append(feature_names[idx])
-    
-    return render_template("home.html", result1=feats)
-    
+                    break
+                
     features = np.array([feats, message])
     non_empty = [x for x in features[1] if x != ""]
     non_empty_idx = [list(features[1]).index(x) for x in non_empty]
